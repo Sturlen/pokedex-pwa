e7b341d756c1440d85921916810d31ea7f93e65d
Sturlen <43607012+Sturlen@users.noreply.github.com>
2020-07-25 21:39:04 +0200

    Add more Mock cards

---
+++ src/interface/mock/PokemonCard.ts
@@ -0,0 +1,24 @@
+import { PokemonInfo } from "../PokemonInfo"
+export const Pikachu: PokemonInfo = {
+  pokedex_nr: 25,
+  name: "Pikachu",
+  types: ["Electric"],
+  flavour_text:
+    "When several of these POKéMON gather, their electricity could build and cause lightning storms.",
+  default_sprite_src: "https://pokeres.bastionbot.org/images/pokemon/25.png",
+}
+
+export const Dialga: PokemonInfo = {
+  pokedex_nr: 485,
+  name: "Dialga",
+  types: ["Dragon", "Steel"],
+  default_sprite_src: "https://pokeres.bastionbot.org/images/pokemon/483.png",
+}
+
+export const BigDummy: PokemonInfo = {
+  pokedex_nr: 12312312312312,
+  name: "How far can a fully laden swallow fly?",
+  types: ["Bug", "Flying", "Fire", "Electric", "Ground", "Dark"],
+}
+
+export default Pikachu
