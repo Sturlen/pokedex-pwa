923b35763ea5c7f613f427574760cac6d630abed
Sturlen <43607012+Sturlen@users.noreply.github.com>
2020-07-28 14:41:05 +0200

    Add types to schema

--- src/api/pokeapico/PokemonSchema.ts
+++ src/api/pokeapico/PokemonSchema.ts
@@ -1,19 +1,29 @@
 import * as z from "zod"
 import { NamedResource } from "./NamedResource"
 import { PokemonInfo } from "../../interface/PokemonInfo"
+import Immutable from "../../interface/Immutable"
+
+export const PokemonTypeSchema = z.object({
+  /** The order the Pokémon's types are listed in. */
+  slot: z.number().int().nonnegative(),
+  type: NamedResource,
+})
 
 export const PokemonSchema = z.object({
   id: z.number().int(),
   name: z.string().nonempty(),
-  //types: NamedResource.array(),
+  types: PokemonTypeSchema.array(),
 })
 
-export type Pokemon = z.infer<typeof PokemonSchema>
+export type Pokemon = Immutable<z.infer<typeof PokemonSchema>>
 
 export function pokemonToPokemonInfo(pkmn: Pokemon): PokemonInfo {
+  const sorted_types = Array.from(pkmn.types)
+    .sort((a, b) => a.slot - b.slot)
+    .map((type_res) => type_res.type.name)
   return {
     pokedex_nr: pkmn.id,
     name: pkmn.name,
-    //types: pkmn.types.map((type) => type.name),
+    types: sorted_types,
   }
 }
