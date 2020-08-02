b04c53d1eea060e23f1a2ec3026af36edda1c732
Sturlen <43607012+Sturlen@users.noreply.github.com>
2020-08-02 11:46:07 +0200

    Add cache config

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
+/* eslint-disable react/prop-types */
 import React from "react"
 import "./App.css"
 import PokemonAPI from "./api/pokeapico"
 import InfiniteScrollList from "./components/InfiniteScrollList"
+import { ReactQueryProviderConfig, ReactQueryConfigProvider } from "react-query"
 
 const api = new PokemonAPI()
 
-function App() {
+const query_config: ReactQueryProviderConfig = {
+  queries: {
+    staleTime: 60 * 60 * 1000, // Do not recheck until 1hr after
+    cacheTime: 24 * 60 * 60 * 1000, // Will store data for up to 24hr
+    refetchOnWindowFocus: false,
+  },
+}
+
+/**
+ * Injects common css and configs
+ */
+export const App: React.FC = () => {
   return (
     <div className="App">
-      <InfiniteScrollList query={api.fetchPokemonInfoList} group_size={15} />
+      <ReactQueryConfigProvider config={query_config}>
+        {
+          <InfiniteScrollList
+            query={api.fetchPokemonInfoList}
+            group_size={15}
+          />
+        }
+      </ReactQueryConfigProvider>
     </div>
   )
 }
