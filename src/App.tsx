8ba7c24b28721bb3ce1b3bc778ab1c6b5b26ed19
Sturlen <43607012+Sturlen@users.noreply.github.com>
2020-08-20 13:56:03 +0200

    Remove AppNavigation from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,7 @@
 import React from "react"
-import { Typography } from "@material-ui/core"
-import PokemonIcon from "@material-ui/icons/Favorite"
-import MovesIcon from "@material-ui/icons/List"
-import AppNavigation from "./components/AppNavigation"
 
 export const App: React.FC = () => {
-  return (
-    <AppNavigation
-      tabs={[
-        {
-          icon: <PokemonIcon />,
-          label: "Pokèmon",
-          content: <Typography variant="h1">{"Pokemon Page"}</Typography>,
-        },
-        {
-          icon: <MovesIcon />,
-          label: "Moves",
-          content: <Typography variant="h1">{"Moves Page"}</Typography>,
-        },
-      ]}
-    ></AppNavigation>
-  )
+  return <></>
 }
 
 export default App
